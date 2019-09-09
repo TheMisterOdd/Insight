7c3d317479cce500be2a2c55a456e27655968108
Álvaro Rodrigo <36777501+AlKiam@users.noreply.github.com>
2019-09-09 14:19:05 +0200

    Update screenshot.c

--- examples/screenshot.c
+++ examples/screenshot.c
@@ -4,13 +4,13 @@ int main(void) {
    
    Window* window = mk_Window(1280, 720, "OpenGL", false);
    
-	while (WindowIsRunning(window)) {
+	while (WindowIsRunning(window)) 
+	{
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 		
 		if (Screenshoot(window)) // Take an screenshoot from the buffer and checks if it was succesfully taken
 			break; // if the screenshot was success, breaks the loop
-
 	}
     
     return 0;
