a1115a9f1401cecf5c597598bd6fc78b62b277ed
Álvaro Rodrigo <36777501+AlKiam@users.noreply.github.com>
2020-05-29 17:21:15 +0200

    Update hello_world.c

--- examples/hello_world.c
+++ examples/hello_world.c
@@ -2,12 +2,14 @@
 
 int main(void) {
    
-   Window* window = mk_Window(1280, 720, "OpenGL", false);
+   	Window w;
+	NewWindow(&w, 1280, 720, "", false);
    
-	while (WindowIsRunning(window)) {
+	while (WindowIsRunning(&w)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 	}
-    
-    return 0;
+    	
+	WindowTerminate(&w);
+    	return 0;
 }
